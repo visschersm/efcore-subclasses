3a32d353587ae5ff1e45cdb9f0f8318427c77b21
Mark Visschers <visschers.m@gmail.com>
2021-12-01 17:11:06 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+
+
+public class ProductContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Products;Trusted_Connection=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(properties => properties.Id);
+            entity.OwnsOne(properties => properties.Limits);
+        });
+    }
+
+    DbSet<Product> Products => Set<Product>();
+
+}
+
+public class PhysicalLimits
+{
+    public int MinWeight { get; /*private*/ set; } = 0;
+    public int MaxWeight { get; /*private*/ set; } = 2000;
+
+    //public PhysicalLimits(int minWeight, int maxWeight) => (MinWeight, MaxWeight) = (minWeight, maxWeight);
+}
+
+public class Product
+{
+    public int Id { get; /*private*/ set; }
+    public PhysicalLimits Limits { get; /*private*/ set; }
+
+    /*public Product(int id, PhysicalLimits limits)
+    {
+        Id = id;
+        Limits = limits;
+    }
+
+    public Product(int id, int minWeight, int maxWeight)
+    {
+        Id = id;
+        Limits = new PhysicalLimits(minWeight, maxWeight);
+    }*/
+}
